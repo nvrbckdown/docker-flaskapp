3d4464f47fad761dd68acce2e58f437e47725ae8
Abdulkodir Yusupov <44323682+nvrbckdown@users.noreply.github.com>
2024-07-31 13:57:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 import os
 class ConfigMessage:
-    def __init__(self, log_level, grpc_port, environment, db_url):
+    def __init__(self, log_level, grpc_port, environment, db_url, author):
         self.log_level = log_level
         self.grpc_port = grpc_port
         self.environment = environment
         self.db_url = db_url
+        self.author = author
 
 class DomainMessage:
     def __init__(self, endpoint, domain, log_level):
@@ -23,15 +24,35 @@ app = Flask(__name__)
 def get_env_variable(key, default_value):
     return os.environ.get(key, default_value)
 
+author = get_env_variable('AUTHOR', 'Not YOU!')
+
 @app.route('/config')
 def get_config():
     log_level = get_env_variable('LOG_LEVEL', 'debug')
     grpc_port = get_env_variable('GRPC_PORT', '8080')
     environment = get_env_variable('ENVIRONMENT', 'dev')
     db_url = get_env_variable('DB_URL', 'postgres://admin:supersecret@10.10.10.1:5432/exam-db')
+    author = get_env_variable('AUTHOR', 'Not YOU!')
 
-    message = ConfigMessage(log_level, grpc_port, environment, db_url)
+    message = ConfigMessage(log_level, grpc_port, environment, db_url, author)
     return jsonify(message.__dict__)
+
+@app.route('/check-ingress', methods=['GET'])
+def check_ingress():
+    res = {
+        'domain': request.headers['Host'],
+        'endpoint': '/check-ingress',
+        'full_path:': request.headers['Host'] + '/check-ingress'
+    }
+    return res
+
+@app.route('/practice', methods=['GET'])
+def practice():
+    res = {
+        'author': author,
+        'message': "Well DONE!"
+    }
+    return res
 
 @app.route('/domain')
 def get_domain():
